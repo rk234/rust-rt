208bf02a4072864bf396426045c8c2e1fb3d19f0
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2023-12-29 19:10:34 -0500

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use raylib::prelude::*;
 mod rendering;
 mod scene;
-
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(640, 480)
         .title("Hello, World")
         .build();
-    let mut img = Image::gen_image_color(640, 480, Color::RED);
-    let mut tex = rl.load_texture_from_image(&thread, &img).expect("Failed to load texture");
+    let img = Image::gen_image_color(640, 480, Color::RED);
+    let tex = rl.load_texture_from_image(&thread, &img).expect("Failed to load texture");
+    
     while !rl.window_should_close() {
         /*img.draw_pixel(rl.get_mouse_x(), rl.get_mouse_y(), Color::BLUE);
         tex = rl.load_texture_from_image(&thread, &img).expect("Failed to load");*/
--- src/rendering/mod.rs
+++ src/rendering/mod.rs
@@ -1,6 +1,7 @@
 use std::ops::Add;
 use crate::scene::Scene;
 use raylib::prelude::*;
+use rand::Rng;
 
 pub const EPSILON: f32 = 0.00001f32;
 
@@ -14,9 +15,25 @@ pub struct RayCamera {
 }
 
 impl RayCamera {
-    pub fn gen_primary_ray(&self) -> Ray {
-        todo!()
-        //return Ray::new(origin, direction)
+    pub fn update_viewport(&mut self, screen_width: usize, screen_height: usize) {
+        self.viewport_size = Vector3::new((screen_width as f32/screen_height as f32)*1.5f32, 1.5f32, 0f32)
+    }
+
+    pub fn gen_primary_ray(&self, screen_x: usize, screen_y: usize, screen_width: usize, screen_height: usize) -> Ray {
+        let mut rng = rand::thread_rng();
+        let adjacent = Vector3::new(0f32, 1f32, 0f32).cross(self.direction).normalized();
+        let local_up = adjacent.cross(self.direction).normalized();
+        let bottom_left = adjacent.scale_by(-self.viewport_size.x as f32 / 2f32).add(local_up.scale_by(-self.viewport_size.y as f32 / 2f32));
+        
+        let dir = bottom_left
+        .add(adjacent.scale_by((self.viewport_size.x) * (screen_x as f32 + rng.gen_range(-0.5f32..0.5f32) / screen_width as f32)))
+        .add(local_up.scale_by((self.viewport_size.y) * (screen_y as f32 + rng.gen_range(-0.5f32..0.5f32) / screen_height as f32)))
+        .add(self.direction.scale_by(self.near_plane)).normalized();
+        
+        return Ray::new(
+            self.position,
+            dir
+        );
     }
 }
 
@@ -86,10 +103,29 @@ impl Renderer<'_> {
     pub fn render_sample(&mut self, width: usize, height: usize, frame_buffer: &mut Framebuffer) {
         for y in 0..frame_buffer.height {
             for x in 0..frame_buffer.width {
-                frame_buffer.accum_pixel(x, y, Vector3::new(1f32, 0f32, 0f32))
+                let ray = self.camera.gen_primary_ray(x, y, width, height);
+                frame_buffer.accum_pixel(x, y, sky_color(ray))
             }
         }
 
         self.num_samples+=1;
     }
+
+    pub fn render_full(&mut self, width: usize, height: usize, frame_buffer: &mut Framebuffer, samples: u32) {
+        for _ in 0..samples {
+            self.render_sample(width, height, frame_buffer);
+        }
+
+        for y in 0..frame_buffer.height {
+            for x in 0..frame_buffer.width {
+                frame_buffer.set_pixel(x, y, frame_buffer.get_pixel(x, y).scale_by(1f32/(self.num_samples as f32)));
+            }
+        }
+    }
+}
+
+
+fn sky_color(ray: Ray) -> Vector3 {
+    let t = 0.5f32*(ray.direction.y + 1.0f32);
+    return Vector3::new(1f32-t, 1f32-t, 1f32-t) + Vector3::new((t * 138f32/255f32), (t*188f32/255f32),t);
 }
